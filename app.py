1ebb5c9fe84fc919b75d333661c79efcacc64e55
anushiya <moorthyanushiya13@gmail.com>
2025-12-28 14:39:39 +0530

    Close temporary file after saving uploaded video
    Ensure temporary file is properly closed after writing.

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ if uploaded_file is not None:
     # Save uploaded video temporarily
     tfile = NamedTemporaryFile(delete=False)
     tfile.write(uploaded_file.read())
+    tfile.close() 
 
     # Open video
     cap = cv2.VideoCapture(tfile.name)
